26bd7bda96ad42d1a543cbfb8a6dc152e2dbeb20
Nico <nicolasmartinalvarez85@gmail.com>
2021-07-13 17:04:01 -0300

    Se agrega primer commit del proyecto

---
+++ JavaScript/principal.js
@@ -0,0 +1,7 @@
+// const button = document.getElementById("button");
+// const myFunction = () => {
+//   const path = document.getElementById("input-file").value;
+//   console.log(path)
+// }
+
+// button.addEventListener("click", myFunction )
