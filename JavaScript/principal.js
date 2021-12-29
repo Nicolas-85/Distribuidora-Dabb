97e5134bf6f1eecb684fee76c2b89ec14338d696
Nicolas-85 <nicolasmartinalvarez85@gmail.com>
2021-12-29 15:08:23 -0300

    Actualización 29-12-21

--- JavaScript/principal.js
+++ JavaScript/principal.js
@@ -1,7 +1,51 @@
-// const button = document.getElementById("button");
-// const myFunction = () => {
-//   const path = document.getElementById("input-file").value;
-//   console.log(path)
-// }
+const clickMouse = document.querySelector('.btnMobile');
+const menuDesplegable = document.querySelector(".contListaNone");
+let visible = false;
 
-// button.addEventListener("click", myFunction )
+clickMouse.addEventListener("click", (e) => {
+    if (visible == false) {
+        menuDesplegable.classList.add("mostrar");
+        visible = true;
+    } else {
+        menuDesplegable.classList = "none";
+        visible = false;
+    }
+  });
+// const menu = document.querySelector('.contListaNone')
+
+menuDesplegable.addEventListener('click', (e) => {
+    if (e.target === menuDesplegable && menuDesplegable.classList.contains("mostrar") || e.target.classList.contains(".anclaLista")) {
+        menuDesplegable.classList.remove("mostrar");  
+    } 
+    menuDesplegable.classList.remove("mostrar");
+})
+
+// const clickMouse = document.querySelector(".btnMobile");
+// let mostrar = false;
+
+// clickMouse.addEventListener('click', function(e) {
+//     if (mostrar == false) {
+//         document.querySelector('.contListaNone').style.display = "block";
+//         mostrar = true;
+//     } else {
+//         document.querySelector('.contListaNone').style.display = "none";
+//     }
+// })
+    
+// const clickMouse = document.querySelector('.btnMobile');
+// const menuDesplegable = document.querySelector(".contListaNone");
+// clickMouse.addEventListener("click", (e) => {
+//     menuDesplegable.classList.add("mostrar");
+//   });
+// // const menu = document.querySelector('.contListaNone')
+
+// menuDesplegable.addEventListener('click', (e) => {
+//     if (e.target === menuDesplegable && menuDesplegable.classList.contains("mostrar") || e.target.classList.contains(".anclaLista")) {
+//         menuDesplegable.classList.remove("mostrar");  
+//     } 
+//     menuDesplegable.classList.remove("mostrar");
+// })
+
+
+
+ 
